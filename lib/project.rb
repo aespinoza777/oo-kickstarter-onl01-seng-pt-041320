799b67b1b86df4f0f3776347a32bc9ecd1c2a9cb
Antonio Espinoza <aespinoza777@gmail.com>
2020-05-21 00:55:32 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+lass Project
+  attr_reader :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    backers << backer
+    backer.backed_projects << self
+  end
+
+end
